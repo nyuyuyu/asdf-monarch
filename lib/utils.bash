f5f85b39bf9f43e6c54c2e36a8c0d8f67a9b129b
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-10-08 00:01:29 +0900

    Implement `list-all`

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for monarch.
 GH_REPO="https://github.com/Dropsource/monarch"
 TOOL_NAME="monarch"
 TOOL_TEST="monarch --help"
@@ -14,11 +13,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if monarch is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
@@ -27,13 +21,12 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -v "\-pre" | grep -v \.pre |
+		grep -E '^(binaries|monarch-binaries)-'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if monarch has other means of determining installable versions.
-	list_github_tags
+	list_github_tags | sed -E 's/^(binaries|monarch-binaries)-//'
 }
 
 download_release() {
