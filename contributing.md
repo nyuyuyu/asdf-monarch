21ba494d8d83e9929e56f619537e30bb366adaa4
nyuyuyu <127820811+nyuyuyu@users.noreply.github.com>
2023-10-08 16:16:16 +0900

    feat: Initial implementation (#3)
    * Implement `list-all`
    
    * Implement `download`
    
    * Implement `install`
    
    * Implement `latest-stable`
    
    * Implement `post-plugin-add`
    
    * Update README.md
    
    * Update contributing.md
    
    * Update LICENSE

--- contributing.md
+++ contributing.md
@@ -3,9 +3,6 @@
 Testing Locally:
 
 ```shell
-asdf plugin test <plugin-name> <plugin-url> [--asdf-tool-version <version>] [--asdf-plugin-gitref <git-ref>] [test-command*]
-
-# TODO: adapt this
 asdf plugin test monarch https://github.com/nyuyuyu/asdf-monarch.git "monarch --help"
 ```
 
--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for monarch.
 GH_REPO="https://github.com/Dropsource/monarch"
 TOOL_NAME="monarch"
 TOOL_TEST="monarch --help"
@@ -12,12 +11,56 @@ fail() {
 	exit 1
 }
 
-curl_opts=(-fsSL)
+monarch_download_base_url() {
+	local version="$1"
 
-# NOTE: You might want to remove this if monarch is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
+	case "$version" in
+	1.*)
+		echo -n "https://d2dpq905ksf9xw.cloudfront.net"
+		;;
+	*)
+		echo -n "https://d148mrbia1nlbw.cloudfront.net"
+		;;
+	esac
+}
+
+platform() {
+	case "$(uname -s)" in
+	"Darwin")
+		echo -n "macos"
+		;;
+	"Linux")
+		echo -n "linux"
+		;;
+	*)
+		fail "Unsupported platform"
+		;;
+	esac
+}
+
+platform_extension() {
+	case "$(uname -s)" in
+	"Linux")
+		echo "tar.xz"
+		;;
+	*)
+		echo "zip"
+		;;
+	esac
+}
+
+tar_decompression_option() {
+	case "$(uname -s)" in
+	"Linux")
+		echo "-xJf"
+		;;
+	*)
+		echo "-xzf"
+		;;
+	esac
+}
+
+curl_opts=(-fsSL)
 
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
@@ -27,13 +70,12 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		grep -v "\-pre" | grep -v \.pre |
+		grep -E '^(binaries|monarch-binaries)-'
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if monarch has other means of determining installable versions.
-	list_github_tags
+	list_github_tags | sed -E 's/^(binaries|monarch-binaries)-//'
 }
 
 download_release() {
@@ -41,8 +83,7 @@ download_release() {
 	version="$1"
 	filename="$2"
 
-	# TODO: Adapt the release URL convention for monarch
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="$(monarch_download_base_url "$version")/$(platform)/monarch_$(platform)_${version}.$(platform_extension)"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -51,7 +92,8 @@ download_release() {
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
+	local bin_path="$install_path/bin"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,10 +103,9 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert monarch executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$bin_path/$tool_cmd" || fail "Expected $bin_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
